e2a480eccb90ec836417a8ff9af09d42c9908c32
Kayli Brownstein <kaylivbrownstein@gmail.com>
2020-12-10 16:11:47 -0500

    Async load google font without plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,13 @@ module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        google: {
-          families: ["Droid Sans", "Droid Serif", "Roboto"],
-        },
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-web-font-loader",
+    //   options: {
+    //     google: {
+    //       families: ["Droid Sans", "Droid Serif", "Roboto"],
+    //     },
+    //   },
+    // },
   ],
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,6 +9,11 @@ const Header = () => (
       <title>Gatsby Silly Site: Guess the dog breed</title>
       <meta name="description" content="Gatsby silly site to guess dog breed" />
       <html lang="en" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap"
+        rel="stylesheet"
+        rel="preload"
+      />
     </Helmet>
     <NavMenu />
   </>
